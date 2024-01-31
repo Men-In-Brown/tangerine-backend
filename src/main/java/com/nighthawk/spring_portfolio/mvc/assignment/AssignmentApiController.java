4fce8b91d40ca67b0fe7509131c21f6abae8f0c9
BobTheFarmer <colinmweis@gmail.com>
2024-01-31 14:41:29 -0800

    Submissions hashmap

--- src/main/java/com/nighthawk/spring_portfolio/mvc/assignment/AssignmentApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/assignment/AssignmentApiController.java
@@ -1,12 +1,16 @@
-package com.nighthawk.spring_portfolio.mvc.assigment;
+package com.nighthawk.spring_portfolio.mvc.assignment;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
+
+import java.util.Map;
 
 @RestController // annotation to simplify the creation of RESTful web services
 @RequestMapping("/api/assignments")  // all requests in file begin with this URI
@@ -40,11 +44,40 @@ public class AssignmentApiController {
             return new ResponseEntity<>("Link is less than 3 or longer than 100 characters", HttpStatus.BAD_REQUEST);
         }
         
-        repository.save(new Assignment(null, title, desc, link));
+        repository.save(new Assignment(title, desc, link));
         return new ResponseEntity<>("Created successfully", HttpStatus.CREATED);
     }
     /* Update Like
      * @PutMapping annotation is used for mapping HTTP PUT requests onto specific handler methods.
      * @PathVariable annotation extracts the templated part {id}, from the URI
      */
+
+    @PostMapping(value = "/submit", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Assignment> personStats(@RequestBody final Map<String,Object> submissions_map) {
+        // find ID
+        long id=Long.parseLong((String)submissions_map.get("id"));  
+        Optional<Assignment> optional = repository.findById((id));
+        if (optional.isPresent()) {  // Good ID
+            Assignment assignment = optional.get();  // value from findByID
+
+            // Extract Attributes from JSON
+            Map<String, Object> attributeMap = new HashMap<>();
+            for (Map.Entry<String,Object> entry : submissions_map.entrySet())  {
+                // Add all attribute other thaN "date" to the "attribute_map"
+                if (!entry.getKey().equals("date") && !entry.getKey().equals("id"))
+                    attributeMap.put(entry.getKey(), entry.getValue());
+            }
+
+            // Set Date and Attributes to SQL HashMap
+            Map<String, Map<String, Object>> date_map = new HashMap<>();
+            date_map.put( (String) submissions_map.get("date"), attributeMap );
+            assignment.setSubmissions(date_map);  // BUG, needs to be customized to replace if existing or append if new
+            repository.save(assignment);  // conclude by writing the stats updates
+
+            // return Person with update Stats
+            return new ResponseEntity<>(assignment, HttpStatus.OK);
+        }
+        // return Bad ID
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST); 
+    }
 }
