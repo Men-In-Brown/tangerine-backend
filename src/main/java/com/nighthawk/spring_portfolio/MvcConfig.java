ecc26889988f6453748a523f85f12f8b1c5182e2
BobTheFarmer <colinmweis@gmail.com>
2024-02-06 12:02:11 -0800

    Security 3

--- src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
@@ -21,10 +21,8 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/volumes/uploads/**").addResourceLocations("file:volumes/uploads/");
     }
 
-    /*
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("men-in-brown.github.io", "http://localhost:4000");
     }
-    */
 }
