6d0035f6bf8f164666ff1e65835d3a0422aa1e5f
Aniket Chakradeo <aniket@chakradeo.net>
2024-02-08 12:03:01 -0800

    jwt api controller file

--- src/main/java/com/nighthawk/spring_portfolio/mvc/jwt/JwtApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/jwt/JwtApiController.java
@@ -10,12 +10,16 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nighthawk.spring_portfolio.mvc.person.Person;
 import com.nighthawk.spring_portfolio.mvc.person.PersonDetailsService;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @CrossOrigin
@@ -30,20 +34,40 @@ public class JwtApiController {
     @Autowired
     private PersonDetailsService personDetailsService;
 
-    @PostMapping("/api/authenticate")
-    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
-        authenticate(authenticationRequest.getEmail(), authenticationRequest.getPassword());
-        final UserDetails userDetails = personDetailsService.loadUserByUsername(authenticationRequest.getEmail());
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest,
+                                                       HttpServletRequest request,
+                                                       HttpServletResponse response) throws Exception {
+        authenticate(authenticationRequest.getGithubUsername(), authenticationRequest.getPassword());
+        final UserDetails userDetails = personDetailsService
+                .loadUserByUsername(authenticationRequest.getGithubUsername());
         final String token = jwtTokenUtil.generateToken(userDetails);
         final ResponseCookie tokenCookie = ResponseCookie.from("jwt", token)
-            .httpOnly(true)
-            .secure(true)
-            .path("/")
-            .maxAge(3600)
-            .sameSite("None; Secure")
-            // .domain("example.com") // Set to backend domain
-            .build();
-        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, tokenCookie.toString()).build();
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(3600)
+                .sameSite("None; Secure")
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE, tokenCookie.toString());
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/signout")
+    public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("jwt")) {
+                    cookie.setValue("");
+                    cookie.setPath("/");
+                    cookie.setMaxAge(0);
+                    response.addCookie(cookie);
+                    break;
+                }
+            }
+        }
+        return ResponseEntity.ok().build();
     }
 
     private void authenticate(String username, String password) throws Exception {
