0c7512f07235c1dcb7778ab2a9a818adafb619b6
Aniket Chakradeo <aniket@chakradeo.net>
2024-02-13 20:15:27 -0800

    working login using mort code

--- src/main/java/com/nighthawk/spring_portfolio/mvc/ModelInit.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/ModelInit.java
@@ -4,14 +4,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.nighthawk.spring_portfolio.mvc.assignment.Assignment;
-import com.nighthawk.spring_portfolio.mvc.assignment.AssignmentJpaRepository;
-import com.nighthawk.spring_portfolio.mvc.grade.Grade;
-import com.nighthawk.spring_portfolio.mvc.grade.GradeJpaRepository;
-import com.nighthawk.spring_portfolio.mvc.issue.Issue;
-import com.nighthawk.spring_portfolio.mvc.issue.IssueJpaRepository;
+import com.nighthawk.spring_portfolio.mvc.jokes.Jokes;
+import com.nighthawk.spring_portfolio.mvc.jokes.JokesJpaRepository;
 import com.nighthawk.spring_portfolio.mvc.note.Note;
 import com.nighthawk.spring_portfolio.mvc.note.NoteJpaRepository;
 import com.nighthawk.spring_portfolio.mvc.person.Person;
@@ -19,78 +16,53 @@ import com.nighthawk.spring_portfolio.mvc.person.PersonDetailsService;
 import com.nighthawk.spring_portfolio.mvc.person.PersonRole;
 import com.nighthawk.spring_portfolio.mvc.person.PersonRoleJpaRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 @Configuration // Scans Application for ModelInit Bean, this detects CommandLineRunner
 public class ModelInit {  
-    @Autowired AssignmentJpaRepository assignmentRepo;
-    @Autowired IssueJpaRepository issueRepo;
-    @Autowired GradeJpaRepository gradeRepo;
     @Autowired NoteJpaRepository noteRepo;
-    @Autowired PersonDetailsService personService;
-    @Autowired PersonRoleJpaRepository roleRepo;
+    @Autowired PersonRoleJpaRepository roleJpaRepository;
+    @Autowired PersonDetailsService personDetailsService;
 
     @Bean
+    @Transactional
     CommandLineRunner run() {  // The run() method will be executed after the application starts
         return args -> {
 
-            // Joke database is populated with starting jokes
-            /*String[] jokesArray = Assigment.init();
-            for (String joke : jokesArray) {
-                List<Assigment> jokeFound = jokesRepo.findByJokeIgnoreCase(joke);  // JPA lookup
-                if (jokeFound.size() == 0)
-                    jokesRepo.save(new Assigment(null, joke, 0, 0)); //JPA save
-            }*/
-            // Add assignment data
-            assignmentRepo.deleteAll();
-            assignmentRepo.save(new Assignment("JQuery Hacks", "Do these JQuery Hacks! Essentially, jQuery is a library that allows us to use some of JavaScript’s built in functions  Benefits of jQuery Benefits of jQuery include:  Makes it easier for us to write JavaScript and HTML code Very flexible in terms of which browsers it can work on Simplifies some of the most common JavaScript functions into fewer lines of code Question: What are some real life applications of jQuery? Name at least two you can think of.  Web pages, used to make dropdown menus appear smoothly Simplifies implementation of AJAX, allows developers to make asynchronous requests to a server and update parts of a web page without requiring a full page reload", "https://nighthawkcoders.github.io/teacher_portfolio//2023/12/07/P1_student_jQuerry_CRUD_IPYNB_2_.html", 2));
-            assignmentRepo.save(new Assignment("HashMap Hacks", "Please do these HashMap Hacks! A HashMap store items in “key/value” pairs, and you can access them by an index of another type (e.g. a String). One object is used as a key (index) to another object (value). It can store different types: String keys and Integer values, or the same type, like: String keys and String values.", "https://nighthawkcoders.github.io/teacher_portfolio//2023/12/13/P3_student_HashmapsHashsetsCollections_IPYNB_2_.html", 3));
-
-            //Issue data
-            issueRepo.save(new Issue("How to change my port?","I want to change my port on the backend to be on port 8085. How can I change it?", "rachit-j", "Sure thing, Rachit-j! To change the port your backend is running on to 8085, you'll need to modify the configuration in your code. The exact steps will depend on the technology stack you're using, but here's a general idea:\n1. Locate the configuration file where the server port is defined. It could be in a `config` folder, or directly in your `server.js` or `app.js"));
-
-            // adding roles
-            PersonRole[] personRoles = PersonRole.init();
-            for (PersonRole role : personRoles) {
-                PersonRole existingRole = roleRepo.findByName(role.getName());
-                if (existingRole != null) {
-                    // role already exists
-                    continue;
-                } else {
-                    // role doesn't exist
-                    roleRepo.save(role);
-                }
-            }
-
-            // Person database is populated with test data
+            // Person database is populated with starting people
             Person[] personArray = Person.init();
             for (Person person : personArray) {
-                //findByNameContainingIgnoreCaseOrEmailContainingIgnoreCase
-                List<Person> personFound = personService.list(person.getName(), person.getEmail());  // lookup
-                if (personFound.size() == 0) {
-                    personService.save(person);  // save
+                // Name and email are used to lookup the person
+                List<Person> personFound = personDetailsService.list(person.getName(), person.getEmail());  // lookup
+                if (personFound.size() == 0) { // add if not found
+                    // Roles are added to the database if they do not exist
+                    List<PersonRole> updatedRoles = new ArrayList<>();
+                    for (PersonRole role : person.getRoles()) {
+                        // Name is used to lookup the role
+                        PersonRole roleFound = roleJpaRepository.findByName(role.getName());  // JPA lookup
+                        if (roleFound == null) { // add if not found
+                            // Save the new role to database
+                            roleJpaRepository.save(role);  // JPA save
+                            roleFound = role;
+                        }
+                        // Accumulate reference to role from database
+                        updatedRoles.add(roleFound);
+                    }
+                    // Update person with roles from role databasea
+                    person.setRoles(updatedRoles); // Object reference is updated
 
-                    // Each "test person" starts with a "test note"
+                    // Save person to database
+                    personDetailsService.save(person); // JPA save
+
+                    // Add a "test note" for each new person
                     String text = "Test " + person.getEmail();
                     Note n = new Note(text, person);  // constructor uses new person as Many-to-One association
-                    noteRepo.save(n);  // JPA Save
-                    personService.addRoleToPerson(person.getEmail(), "ROLE_STUDENT");
+                    noteRepo.save(n);  // JPA Save                  
                 }
             }
-            // for lesson demonstration: giving admin role to Mortensen
-            personService.addRoleToPerson(personArray[4].getEmail(), "ROLE_ADMIN");
 
-            //delete all entries from grade database
-            gradeRepo.deleteAll();
-            Grade[] gradeArray = Grade.init();
-            for (Grade score : gradeArray) {
-                //List<Grade> test = gradeRepo.list(score.getName());  // lookup
-                //if (test.size() == 0) {
-                    gradeRepo.save(score);
-                //};
-            }
         };
     }
 }
-
